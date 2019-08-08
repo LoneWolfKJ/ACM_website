afb71ad176ecc92a1f9c715532bca48fce5e7e5d
Kanjal Dalal <kanjaldalal1000@gmail.com>
2019-08-08 06:40:54 +0530

    update events

--- script.js
+++ script.js
@@ -96,9 +96,24 @@ $(window).on('scroll',function(){
     x=$(window).scrollTop();
     }
 });
+// Get the modal
+var modal = document.getElementById("myModal1");
 
-function getId(element)
-{
-    console.log("hello");
-    location.href="./articleonclick.html";
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("list-item");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("overlay");
+
+// When the user clicks on the button, open the modal 
+function on(x) {
+  document.getElementById("overlay").style.display = "block";
+  $('.pop-box').empty();
+  $('#event-'+x).clone().appendTo('.pop-box');
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  document.getElementById("overlay").style.display = "none";
 }
